d7fef9a4d79e1554685f9ce3a29ab2c01971eac8
Trent Endresen <trentendresen@Trents-MacBook-Pro.local>
2022-10-13 12:59:14 -0500

    Added auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
 
+import config from "./aws-exports";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+Amplify.configure(config);
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
